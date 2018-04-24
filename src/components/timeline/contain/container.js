646f17b37e9c5128048e64de0afde1acfd4ea061
my <741967556@qq.com>
2018-04-24 10:25:09 +0800

    FIRST COMMIT

--- src/components/timeline/contain/container.js
+++ src/components/timeline/contain/container.js
@@ -437,10 +437,10 @@ class Container extends Component {
                     var minIndex = getMinIndex(arr);//找到最小的索引
 
                     oItem[i].style.position = "absolute";//加定位
-                    oItem[i].style.top = arr[minIndex] + "px";  //top的位置
-                    oItem[i].style.left = oItem[i].offsetWidth * minIndex + "px";
+                    oItem[i].style.top = arr[minIndex] +Math.floor(i/4)*10+ "px";  //top的位置
+                    oItem[i].style.left = oItem[i].offsetWidth * minIndex + minIndex*10  + "px";
 
-                    // oItem[i].style.marginRight="10px";
+
                     //把最矮高度，改变
                     arr[minIndex] = arr[minIndex] + oItem[i].offsetHeight;
                 }
